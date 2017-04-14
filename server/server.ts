54b2790e0e4ad858027dd7bb2308fb3f662751ea
freeman29 <tordafrancis@gmail.com>
2017-04-14 12:32:32 +0800

    add express add courses route

--- server/server.ts
+++ server/server.ts
@@ -1,3 +1,15 @@
 import { findAllCourses } from "./queries/findAllCourses";
+import * as express from 'express';
+import { Application } from 'express';// type interface of express
 
-findAllCourses().then(results => console.log(JSON.stringify(results, null, 2)));
+const app: Application = express();
+const port: number = 8090;
+
+app.route('/api/courses')
+	.get((req, res) => {
+		findAllCourses().then(results => res.status(200).json(results));
+	})
+
+app.listen(port, () => {
+	console.log(`Server running on localhost:${port}`);
+})
