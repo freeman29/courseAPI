472fe3d4a7192f52b1de13dd4b9f31c062846380
freeman29 <tordafrancis@gmail.com>
2017-04-14 20:55:53 +0800

    add route for course details

--- server/api/api.ts
+++ server/api/api.ts
@@ -1,7 +1,9 @@
 import * as express from 'express';
 import { Application } from 'express';// type interface of express
 import { apiGetAllCourses } from './apiGetAllCourses';
+import { apiGetCourseDetails } from './apiGetCourseDetails';
 
 export const initRestApi = (app: Application) =>  {
 	app.route('/api/courses').get(apiGetAllCourses);
+	app.route('/api/courses/:id').get(apiGetCourseDetails);
 };
