7c0e1455bc39b5409e0a373a2a2a6aaf573b2b0a
freeman29 <tordafrancis@gmail.com>
2017-04-14 19:34:24 +0800

    add middleware for error

--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,14 @@
-import { findAllCourses } from "./queries/findAllCourses";
 import * as express from 'express';
-import { Application } from 'express';// type interface of express
+import { Application } from 'express';// type interface of express Application
+import { initRestApi } from "./api/api";
+import { apiErrorHandler } from './api/apiErrorHandler';
 
 const app: Application = express();
 const port: number = 8090;
 
-app.route('/api/courses')
-	.get((req, res) => {
-		findAllCourses().then(results => res.status(200).json(results));
-	})
+initRestApi(app);
+
+app.use(apiErrorHandler);
 
 app.listen(port, () => {
 	console.log(`Server running on localhost:${port}`);
