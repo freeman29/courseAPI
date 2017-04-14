71127104999b599a6453df0d5feefb1e84b6e5db
freeman29 <tordafrancis@gmail.com>
2017-04-14 21:01:33 +0800

    add lessong model and join logic

--- server/models/models.ts
+++ server/models/models.ts
@@ -1,9 +1,14 @@
 import * as ORM from 'sequelize';
 import {Sequelize} from 'sequelize'; // type interface of sequelize
 import { initCourseModel } from './initCourseModel';
+import { initLessonModel } from './initLessonModel';
 
 const DB_URL = 'postgres://postgres:postgres@localhost:5432/complete-typescript-course'; 
 
 const sqlz : Sequelize = new ORM(DB_URL);
 
 export const CourseModel = initCourseModel(sqlz);
+export const LessonModel = initLessonModel(sqlz);
+
+CourseModel.hasMany(LessonModel, {foreignKey: 'courseId'});
+LessonModel.belongsTo(CourseModel, {foreignKey: 'courseId'});
